d5fd8ac45fbb384dab66b8034328d1b95474ae40
Xeon <jjeeraldo@gmail.com>
2018-07-15 21:30:21 +0800

    COMMENT

--- appViews/views_splash.php
+++ appViews/views_splash.php
@@ -1,25 +1,17 @@
 <div id="pages_splash" class="page animated slideInRight" style="animation-duration:.5s!important; overflow: hidden; z-index: 500 !important;width: 100%; padding-top: 0px; height:100vh; animation-delay:.5s">
 
-    <!--<img src="dependencies/images/logo.svg" alt="" class="w3-display-topleft" style="width:64px; padding-left:25px; padding-top:25px;" />-->
-    <!--<h5 class="w3-display-topright" style="text-align:center; margin-bottom:100px;padding-right:25px; padding-top:20px; font-style:italic" onclick="show_view('pages_login')">skip</h5>-->
-    
-    <!--<br />
-    <br />-->
-    <div class="w3-display-middle" style="width:70%; max-height:350px">
-        <img src="dependencies/images/logo.svg" id="splash_logo" alt="" class="animated pulse infinite" />
-    </div>
-    
-    <h3 class="w3-display-bottommiddle" style="text-align:center; margin-bottom:100px;padding:0px;width:300px;"></h3>
+  
+    <!--<div id="pageMenu_login" class="page_menu" style="animation-duration: .5s; background-color: rgba(0, 0, 0, 0); padding-top:35px; padding-left:15px">
+        <i class="fa fa-chevron-left" onclick="close_view('pages_login','YES');" style="color:white; font-size:24px"></i>
+        <div style="display: inline-block; position:absolute; margin-top:4px; margin-left:5px">
+            <h6 onclick="close_view('pages_login');" style="display: inline; color: white; font-size: 17px"></h6>
+        </div>
 
-    <h1 class="w3-display-bottommiddle" style="text-align:center; margin-bottom:120px;padding:0px;width:300px;">Hanap</h1>
-    <h5 class="w3-display-bottommiddle" style="text-align:center; margin-bottom:60px;padding:0px;width:300px;">Or swipe up to learn more</h5>
-   
-
- 
-    <!--<div id="feed_body" class="feed_body"    style="-webkit-animation-duration: .8s !important; padding:0; margin-top: 125px">
-
-       
     </div>-->
+
+  
+
+
   
 
 
