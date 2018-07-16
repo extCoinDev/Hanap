5727a005296b427467aa42f42f985b4793e351dd
Dexter <ugsimardexter.du@gmail.com>
2018-07-16 01:08:37 -0700

    HI GUYS

---
+++ appViews/views_Name.php
@@ -0,0 +1,42 @@
+<div id="pages_EnterName" class="page animated slideInRight" style="animation-duration: .5s!important;
+    overflow: hidden; z-index: 500 !important; width: 100%; padding-top: 0px; height: 100vh;
+    animation-delay: .5s">
+    
+    
+    
+    <!--SIGN UP-->
+    <div class=" w3-display-container " style="background-color: 2196F3; height: 50%; width: 100%; padding: 25px;">
+        <i class="fa fa-arrow-left" style="font-size: 20px; color: White;"></i>
+        <h6 class="" style="color: White; margin-top: 15px; font-size: 20px;margin-left:auto;margin-right:auto;display:block;">
+            Sign Up
+        </h6>
+       
+        <!--ENTER YOUR NAME-->
+        <div class="" style="padding-right: 20px; margin-left: auto; margin-right: auto;display:block;padding-bottom:50px;padding-left:10px;margin-bottom:auto;">
+            <h1 class="" style="font-size: 45px; color: white">
+                Enter Your Name
+            </h1>
+        </div>
+       
+        <!--CIRCLES-->
+        <div class="w3-display-bottomright" style="margin-bottom: 25px;margin-right:25px;margin-left:auto;display:block;padding-top:20px;">
+            
+            <i class="fa fa-circle" style="font-size: 10px; color: White;margin-right:15px;"></i>
+            <i class="fa fa-circle" style="font-size: 10px; color: #ffffff7d;margin-right:15px;"></i>
+            <i class="fa fa-circle" style="font-size: 10px; color: #ffffff7d;margin-right:15px;"></i>
+            <i class="fa fa-circle" style="font-size: 10px; color: #ffffff7d;margin-right:15px;"></i>
+            <i class="fa fa-circle" style="font-size: 10px; color: #ffffff7d;margin-right:15px;"></i>
+            <i class="fa fa-circle" style="font-size: 10px; color: #ffffff7d;margin-right:15px;"></i>
+            <i class="fa fa-circle" style="font-size: 10px; color: #ffffff7d;margin-right:15px;"></i>
+            
+      
+        </div>
+    </div>
+    <!--<div id="pageMenu_login" class="page_menu" style="animation-duration: .5s; background-color: rgba(0, 0, 0, 0); padding-top:35px; padding-left:15px">
+        <i class="fa fa-chevron-left" onclick="close_view('pages_login','YES');" style="color:white; font-size:24px"></i>
+        <div style="display: inline-block; position:absolute; margin-top:4px; margin-left:5px">
+            <h6 onclick="close_view('pages_login');" style="display: inline; color: white; font-size: 17px"></h6>
+        </div>
+
+    </div>-->
+</div>
---
+++ appViews/views_SlidingPagesBLUE.php
@@ -0,0 +1,54 @@
+<div id="pages_SlidePage_2" class="page animated slideInRight" style="animation-duration:.5s!important; background-color:#2196F3; overflow: hidden; z-index: 500 !important;width: 100%; padding-top: 0px; height:100vh;animation-delay:.5s">
+
+    <h6 class="" style="color:White;font-size:20px;text-align:right;margin-left:auto;margin-right:30px;margin-top:50px;display:block"> Skip </h6>
+
+    <!--LOGO-->
+    
+    <div class="w3-display-middle"style="width:100%;">>
+   
+    <div class="w3-display-top">
+     <img src="dependencies/images/logo_outline.svg" style="margin-left:auto;margin-right:auto;margin-top:30px;display:block" >
+    </div>
+   
+
+    <!--CONTENT-->
+    
+    <div class="" style="padding-top:25px;">
+    
+    <h2 class="" style="color:White;font-size:40px;margin-left:auto;margin-right:auto;margin-top:10px; display:block;text-align:center;" > DEXTER</h2>
+
+    </div>
+
+    
+    <!--CIRCLE-->
+    
+    <div class="" style="padding-left:25px;padding-right:25px;padding-top:2vh;">
+    
+    
+    <p style="text-align:center;color:White;font-size:16px;display:block;margin-left:auto;margin-right:auto;margin-top:10px;display:block"" > Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+
+    </div>
+    
+    </div>
+   <div class="w3-display-bottommiddle" style="margin-bottom:2vh;">
+   <i class="fa fa-circle" style="color:#ffffff7d;"></i> 
+<i class="fa fa-circle" style="color:#ffffff7d;"></i> 
+<i class="fa fa-circle" style="color:White;"></i> 
+   </div>
+
+
+    <!--<div id="pageMenu_login" class="page_menu" style="animation-duration: .5s; background-color: rgba(0, 0, 0, 0); padding-top:35px; padding-left:15px">
+        <i class="fa fa-chevron-left" onclick="close_view('pages_login','YES');" style="color:white; font-size:24px"></i>
+        <div style="display: inline-block; position:absolute; margin-top:4px; margin-left:5px">
+            <h6 onclick="close_view('pages_login');" style="display: inline; color: white; font-size: 17px"></h6>
+        </div>
+
+    </div>-->
+
+  
+       
+
+  
+
+
+</div>
