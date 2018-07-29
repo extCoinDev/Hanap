ea9cbb258ee7c1ad79ce13ed092f677c1ed72a81
Quennie <quennierbundag@gmail.com>
2018-07-29 19:41:39 +0800

    Products

---
+++ appViews/views_prod-archs.php
@@ -0,0 +1,26 @@
+<div id="pages_prod-archs" class="page animated slideInRight" style="animation-duration:.5s!important; overflow: hidden; z-index: 500 !important;width: 100%; padding-top: 0px; height:100vh; animation-delay:.5s">
+
+  
+    <!--<div id="pageMenu_login" class="page_menu" style="animation-duration: .5s; background-color: rgba(0, 0, 0, 0); padding-top:35px; padding-left:15px">
+        <i class="fa fa-chevron-left" onclick="close_view('pages_login','YES');" style="color:white; font-size:24px"></i>
+        <div style="display: inline-block; position:absolute; margin-top:4px; margin-left:5px">
+            <h6 onclick="close_view('pages_login');" style="display: inline; color: white; font-size: 17px"></h6>
+        </div>
+
+    </div>
+        -->
+    <div class="w3-card-4" style="background-color: #FFCA28; height: 10%; width:100%;margin-top:-20px;text-align:center;padding-top:5px;color:white;">
+
+        <div class="" style="font-family:Arial">
+            <h5 class="" style="margin-top:25px"><b> PRODUCTS</b></h5>
+            <h4 class="" style="margin-top:-12px"><b>ARCHITECTURES</b></h4>
+        </div>
+    </div>
+    <div class="w3-round-large" style="background-color:black;height:40%;width:95%;margin-top:10px;margin-left:auto;margin-right:auto;display:block">
+    </div>
+
+    <div class="w3-round-large" style="background-color:black;height:40%;width:95%;margin-top:10px;margin-left:auto;margin-right:auto;display:block">
+    </div>
+
+   
+</div>
