e98c56c8ee477319f4428603bb2fcf82193a03d1
Quennie <quennierbundag@gmail.com>
2018-07-30 01:40:12 +0800

    Account Info & Setting
    Account Info (interface4)
    Account Settings (interface3)

--- appViews/views_inteface3.php
+++ appViews/views_inteface3.php
@@ -1,6 +1,6 @@
 <div id="pages_interface3" class="page animated slideInRight" style="animation-duration:.5s!important; overflow: hidden; z-index: 500 !important;width: 100%; padding-top: 0px; height:100vh; animation-delay:.5s; background-color:white;">
 
-  
+
     <!--<div id="pageMenu_login" class="page_menu" style="animation-duration: .5s; background-color: rgba(0, 0, 0, 0); padding-top:35px; padding-left:15px">
         <i class="fa fa-chevron-left" onclick="close_view('pages_login','YES');" style="color:white; font-size:24px"></i>
         <div style="display: inline-block; position:absolute; margin-top:4px; margin-left:5px">
@@ -13,41 +13,55 @@
     <div>
         <div class="w3-display-container" style="background-color:#FFCA28;width:100%;height:33%;border-bottom-right-radius:45%;border-bottom-left-radius:45%;">
 
-   
+
 
         </div>
 
         <!--Arrow-->
-        <div class="w3-display-topleft w3-circle w3-border w3-border-white" style="font-size:30px;color:white;margin-left:15px;margin-right:auto;margin-top:15px;padding:11px">
+        <div class="w3-display-topleft w3-circle w3-border w3-border-white" style="font-size:22px;color:white;margin-left:15px;margin-right:auto;margin-top:15px;padding:5px">
             <i class="fa fa-arrow-left"></i>
         </div>
 
 
 
         <!--Middle-->
-        <div class="w3-display-topmiddle w3-circle w3-white" style="font-size:15px;color:black;margin-top:5%;padding:11px">
 
-            <img src="dependencies/images/123.png" class="w3-circle" style="width: 200px; height: 172px">
+        <div class="w3-display-topmiddle w3-circle w3-white" style="margin-top:5%">
 
+            <i class="fa fa-user" style="color:black;padding:30px;font-size:130px"></i>
         </div>
 
         <div>
             <h1 style="color:black; display: block; margin-left: 26%; margin-right: auto;margin-top:4% ">Sora Shiro</h1>
-            <p style="color:grey; display: block; margin-left: auto; margin-right: 77px;margin-top:-8% ; font-size:10px;text-align:right">Edit</p>
-            <p style="color:grey; display: block; margin-left: auto; margin-right:0%;margin-top:2% ; font-size:12px;text-align:center">Manila, Philippines</p>
+            <p style="color: #757575; display: block; margin-left: auto; margin-right: 77px; margin-top: -8%; font-size: 10px; text-align: right; ">Edit</p>
+            <p style="color: #757575; display: block; margin-left: auto; margin-right: 0%; margin-top: 2%; font-size: 12px; text-align: center; ">Manila, Philippines</p>
         </div>
-       
+
 
         <!--ACCOUNT-->
-        <div class="w3-card-12 w3-center" style="width:90%;height:40%">
-            <h2 style="color:black; display: block; margin-left: auto; margin-right: auto;margin-top:4% ;">ACCOUNT</h2>
+        <div class="w3-card-2 w3-round-medium" style="width: 90%; height: 40%; margin-left: 20px; margin-right: auto">
+            <h4 style="color: black; padding-top: 35px;padding-left:10px"><b>ACCOUNT</b></h4>
+            <p style="color: #757575; margin-left: auto; margin-right: 17px; margin-top: -8%; font-size: 12px; text-align: right; ">Edit</p>
+            
+            <div class="w3-card-2 w3-round-medium" style="width: 90%; height: 10%; margin-left: 15px; margin-right: auto; margin-top:20px">
+                <p style="color: #757575; font-size: 13px; padding-top: 7px; padding-left: 10px; ">Email: SoraShiro@gmail.com</p>
+            </div>
+            
+            <div class="w3-card-2 w3-round-medium" style="width: 90%; height: 10%; margin-left: 15px; margin-right: auto; margin-top: 20px">
+                <p style="color: #757575; font-size: 13px; padding-top: 7px; padding-left: 10px; ">Password: *************</p>
+            </div>
+            
+            <div class="w3-card-2 w3-round-medium" style="width: 90%; height: 10%; margin-left: 15px; margin-right: auto; margin-top: 20px">
+                <p style="color: #757575; font-size: 13px; padding-top: 7px; padding-left: 10px; ">Contact No. : 0975-242-2314</p>
+            </div>
+           
+            <div class="w3-card-2 w3-round-medium" style="width: 90%; height: 10%; margin-left: 15px; margin-right: auto; margin-top: 20px">
+                <p style="color: #757575; font-size: 13px; padding-top: 7px; padding-left: 10px; ">Address: Golden City, Imus Cavite, Cavite</p>
+            </div>
 
 
 
-        </div>
         
-
-
 
 
 
---
+++ appViews/views_inteface4.php
@@ -0,0 +1,86 @@
+<div id="pages_interface4" class="page animated slideInRight" style="animation-duration:.5s!important; overflow: hidden; z-index: 500 !important;width: 100%; padding-top: 0px; height:100vh; animation-delay:.5s; background-color:white;">
+
+
+    <!--<div id="pageMenu_login" class="page_menu" style="animation-duration: .5s; background-color: rgba(0, 0, 0, 0); padding-top:35px; padding-left:15px">
+        <i class="fa fa-chevron-left" onclick="close_view('pages_login','YES');" style="color:white; font-size:24px"></i>
+        <div style="display: inline-block; position:absolute; margin-top:4px; margin-left:5px">
+            <h6 onclick="close_view('pages_login');" style="display: inline; color: white; font-size: 17px"></h6>
+        </div>
+
+    </div>
+        -->
+
+    <div>
+        <div class="w3-display-container" style="background-color:#FFCA28;width:100%;height:33%;border-bottom-right-radius:45%;border-bottom-left-radius:45%;">
+
+
+
+        </div>
+
+        <!--Arrow-->
+        
+            <div class="w3-display-topleft w3-circle w3-border w3-border-white" style="font-size:22px;color:white;margin-left:15px;margin-right:auto;margin-top:15px;padding:5px">
+                <i class="fa fa-arrow-left"></i>
+            </div>
+
+
+
+        <div class="w3-display-topright" style="padding-top:20px;padding-right:20px">
+            <p style="color:white; font-size: 16px; text-align: right; ">Edit</p>
+        </div>
+            
+
+        
+
+
+
+        <!--Middle-->
+
+        <div class="w3-display-topmiddle w3-circle w3-white" style="margin-top:5%">
+
+            <i class="fa fa-user" style="color:black;padding:30px;font-size:130px"></i>
+        </div>
+
+        <div>
+            <h1 style="color:black; display: block; margin-left: 26%; margin-right: auto;margin-top:4% ">Sora Shiro</h1>
+            
+            <p style="color: #757575; display: block; margin-left: auto; margin-right: 0%; margin-top: 2%; font-size: 12px; text-align: center; ">Manila, Philippines</p>
+        </div>
+
+
+        <!--ACCOUNT-->
+        <div class="w3-card-2 w3-round-medium" style="width: 90%; height: 40%; margin-left: 20px; margin-right: auto; padding-top:25px">
+           
+            
+            <div class="w3-card-2 w3-round-medium w3-center" style="width: 90%; height: 10%; margin-left: 15px; margin-right: auto; margin-top:20px">
+                <p style="color: #757575; font-size: 13px; padding-top: 7px; padding-left: 10px; ">SoraShiro@gmail.com</p>
+            </div>
+            
+            <div class="w3-card-2 w3-round-medium w3-center" style="width: 90%; height: 10%; margin-left: 15px; margin-right: auto; margin-top: 20px">
+                <p style="color: #757575; font-size: 13px; padding-top: 7px; padding-left: 10px; ">*************</p>
+            </div>
+            
+            <div class="w3-card-2 w3-round-medium w3-center" style="width: 90%; height: 10%; margin-left: 15px; margin-right: auto; margin-top: 20px">
+                <p style="color: #757575; font-size: 13px; padding-top: 7px; padding-left: 10px; ">0975-242-2314</p>
+            </div>
+           
+            <div class="w3-card-2 w3-round-medium w3-center" style="width: 90%; height: 10%; margin-left: 15px; margin-right: auto; margin-top: 20px">
+                <p style="color: #757575; font-size: 13px; padding-top: 7px; padding-left: 10px; ">Golden City, Imus Cavite, Cavite</p>
+            </div>
+
+
+
+        </div>
+
+
+        <div class="w3-display-bottommiddle w3-display-container" style="background-color:#FFCA28;width:100%;height:5%">
+
+        </div>
+
+
+
+    </div>
+
+
+
+</div>
