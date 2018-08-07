d7b7c2beca59fa282c1371c943e44e01b33d40f8
Karla <quebec.kp@gmail.com>
2018-08-08 04:19:09 +0800

    search and sidebar

--- appViews/views_search_interface.php
+++ appViews/views_search_interface.php
@@ -1,12 +1,69 @@
 <div id="pages_search_interface" class="page animated slideInRight" style="animation-duration:.5s!important; overflow: hidden; z-index: 500 !important;width: 100%; padding-top: 0px; height:100vh; background-color:white;">
 
-
-    <div id="pageMenu_login" class="page_menu" style="animation-duration: .5s; background-color: #FFCA28; padding-top:35px; padding-left:15px">
-        <i class="fa fa-chevron-left" onclick="close_view('pages_search_interface','YES');" style="color:white; font-size:24px"></i>
-        <div style="display: inline-block; position:absolute; margin-top:4px; margin-left:5px">
-            <h6 onclick="close_view('pages_search_interface');" style="display: inline; color: white; font-size: 17px"></h6>
+    <!--BACK AND SEARCH-->
+    <div id="pageMenu_login w3-display-container" class="page_menu" style="animation-duration: .5s; background-color: #FFCA28; padding-top:25px; max-height:110px; padding-bottom: 10px; ">
+       
+       <div class="w3-row w3-display-container">
+       
+         <div class="w3-display-left" style="padding-left:15px;">
+             <i class="fa fa-chevron-left" onclick="close_view('pages_search_interface','YES');" style="color:white; font-size:24px; margin-left:auto; margin-right:auto; text-align:center; display:block;"></i>
+                <!--<div style="display: inline-block; position:absolute; margin-top:4px; margin-left:5px">
+                     <h6 onclick="close_view('pages_search_interface');" style="display: inline; color: white; font-size: 17px"></h6>
+                </div>-->       
+        </div>
+       
+        <div class="w3-col s11 m12 l12" style="padding-right:15px;padding-left:15px; float:right">
+            <div class="w3-round-xxlarge w3-white" style="max-width:600px; min-width:200px;margin-right:auto; margin-left:auto; display:block;" >          
+                <i class="fa fa-search" style="padding-left:15px;color:#ccc;margin-top:10px;margin-bottom:10px"></i>
+                <input class="w3-border-0" id="search" placeholder="Search.." style="width:80%;margin-right:auto;display:inline;line-height:28px;outline:none">
+            </div> 
         </div>
 
+
+       </div>
+               
+    </div>
+
+    <!--RECENT SEARCHES-->
+    <div id="page_recent_search w3-display-container" class="page_recent w3-card-2" style="animation-duration: .5s; background-color: white; padding-top:110px;">
+        <h6 class ="w3-display-topleft" style=" position:absolute; margin-top:80px;margin-left:10px;color: gray; font-size: 13px;padding-top:10px">Recent searches</h6>
+    </div>
+
+    <!--RECENT ITEMS-->
+    <div class="w3-container" style="padding-top:25px">
+        <div class="w3-row w3-center" style="display: block; position: absolute; margin-left: 10px; margin-right:25px">
+            <div class="w3-third w3-center" onclick="" style="padding-right:5px; padding-left:5px">
+                 <p style="color: white; background-color: #FFCA28; border-radius: 10px;"> Laptop </p>
+            </div>
+
+            <div class="w3-third w3-center" onclick="" style="padding-right:5px; padding-left:5px;">
+                 <p style="color: white; background-color: #FFCA28; border-radius: 10px;"> T-Shirt </p>
+            </div>
+
+            <div class="w3-third w3-center" onclick="" style="padding-right:5px; padding-left:5px;">
+                 <p style="color: white; background-color: #FFCA28; border-radius: 10px;"> Asus Rog7 </p>
+            </div>
+
+            <div class="w3-third w3-center" onclick="" style="padding-right:5px; padding-left:5px;">
+                <p style="color: white; background-color: #FFCA28; border-radius: 10px;"> Iphone 6 </p>
+            </div>
+
+            <div class="w3-third w3-center" onclick="" style="padding-right:5px; padding-left:5px">
+                <p style="color: white; background-color: #FFCA28; border-radius: 10px;"> Whirlpool Refrigerator </p>
+            </div>
+
+            <div class="w3-third w3-center" onclick="" style="padding-right:5px; padding-left:5px; ">
+                <p style="color: white; background-color: #FFCA28; border-radius: 10px;"> Philip Television </p>
+            </div>
+
+            <div class="w3-third w3-center" onclick="" style="padding-right:5px; padding-left:5px;">
+                <p style="color: white; background-color: #FFCA28; border-radius: 10px;"> 24 Gold Watch </p>
+            </div>
+
+            <div class="w3-third w3-center" onclick="" style="padding-right:5px; padding-left:5px;">
+                <p style="color: white; background-color: #FFCA28; border-radius: 10px;"> Flip Case </p>
+            </div>
+        </div>
     </div>
 
 
