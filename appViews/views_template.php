5989d0ffa6390a46270cfc0910e8599518e6909b
Xeon <jjeeraldo@gmail.com>
2018-08-11 16:31:06 +0800

    TEMPLATE CHANGES

--- appViews/views_template.php
+++ appViews/views_template.php
@@ -1,19 +1,29 @@
-<div id="pages_history" class="page animated slideInRight  w3-col m8 l9">
-    <div class="page_bkg" style="background-color: #FFA000">
-        <div class="page_menu">
-            <h5 style="margin-left:auto; position: absolute; margin-right:auto; text-align:center; display:inline-block; width:100%; font-size:20px; font-weight:600; margin-top:3px;">Transactions</h5>
+<div id="pages_main" class="animated slideInRight w3-col m8 l9" style="display: none; -webkit-animation-duration: .5s !important; float:right">
+   
+    <!--PAGE MENU--> 
+    <div class="page_menu" style="background-color: #FFA000; ">
+        <!--MENU TITLE--> 
+        <h5 style="margin-left:auto; position: absolute; margin-right:auto; text-align:center; display:inline-block; width:100%; font-size:20px; font-weight:600; margin-top:3px;; margin-top:3px; margin-top:3px;">Dashboard</h5>
 
-            <div style=" position:absolute ; width:100%; left:0; padding-right:15px;padding-left:15px">
-                <i class="fa fa-chevron-left" onclick="close_view('pages_history');" style=" color: white; font-size: 24px; margin-top: 4px; float: left;"></i>
-                <!--<i class="fa fa-user" onclick="showProfile();" style="color: white; font-size: 24px; float: right; margin-top: 5px; "></i>-->
+        <div style=" position:absolute ; width:100%; left:0; padding-right:15px;padding-left:15px">
+            
+            <!--MENU BACK BUTTON--> 
+            <i class="fa fa-chevron-left" onclick="close_view('PAGE_ID')" style="color: white; font-size: 24px; float: left; margin-top: 5px; "></i>
 
-            </div>
+            <!--MENU 2 BUTTON--> 
+            <!--<i class="fa fa-user" onclick="showProfile();" style="color: white; font-size: 24px; float: right; margin-top: 5px; "></i>-->
 
-        </div>
-
-        <div id="feed_body" class="feed_body" style="-webkit-animation-duration: .8s !important">
-
-          
         </div>
     </div>
+    <!--PAGE MENU END--> 
+
+
+ <!--BODY-->
+<div id="feed_body" class="feed_body w3-centered w3-center" style="overflow-x:hidden">
+       
+
+
+</div>
+ <!--BODY END-->
+
 </div>
--- appViews/views_template_dashboard.php
+++ appViews/views_template_dashboard.php
@@ -1,4 +1,6 @@
 <div id="pages_main" class="animated slideInRight w3-col m8 l9" style="display: none; -webkit-animation-duration: .5s !important; float:right">
+   
+    <!--PAGE MENU--> 
     <div class="page_menu" style="background-color: #FFA000; ">
         <h5 style="margin-left:auto; position: absolute; margin-right:auto; text-align:center; display:inline-block; width:100%; font-size:20px; font-weight:600; margin-top:3px;; margin-top:3px; margin-top:3px;">Dashboard</h5>
 
@@ -9,48 +11,10 @@
         </div>
     </div>
 
- <!--FEED BODY-->
+ <!--BODY-->
 <div id="feed_body" class="feed_body w3-centered w3-center" style="overflow-x:hidden">
-        <!--<h6 style="text-align: center; color: #BDBDBD; margin-top:15px">No threads available</h6>-->
-        <div id="" class="w3-center w3-centered dash_temp" style=" display: block;height: 100%; margin: 0 auto; ">
-            
-            <!--<div class="w3-container w3-round w3-center w3-card-2 animated fadeInUp w3-col m5 l3  tiles_set" onclick="showMsg('This is your total mining power in Mh/s from you and all your downlines','Total Hash Power', 'Okay');" style="-webkit-animation-delay: 0s; background-color: rgb(255, 201, 111); color: white; margin-top: 10px; ">
-                <h4 class="tileTitle">Total Hash Power</h4>
-                <h2><b id="myDaily">0.0 BTC</b></h2>
-                <h5 id="tltNoGenDonation" style="color: rgba(0, 0, 0, 0.33);"></h5>
-            </div>
+       
 
-            <div class="w3-container w3-round w3-center w3-card-2 animated fadeInUp w3-col m5 l3  tiles_set" onclick="showMsg('This is your total reward in BTC from your mining power', 'Total BTC Daily', 'Okay');" style="-webkit-animation-delay: 0.1s; background-color: rgb(255, 201, 111); color: white; margin-top: 10px;">
-                <h4 class="tileTitle">Total BTC Daily</h4>
-                <h2><b id="overview_genBonus">0.0 BTC</b></h2>
-                <h5 style="color: rgba(0, 0, 0, 0.33);" id="shared_donation_desc"></h5>
-            </div>
-
-            <div class="w3-container w3-round w3-center w3-card-2 animated fadeInUp w3-col m5 l3  tiles_set" onclick="showMsg('This is your total balance in your wallet', 'Wallet Balance', 'Okay');" style="-webkit-animation-delay: 0.1s; background-color: rgb(255, 201, 111); color: white; margin-top: 10px;">
-                <h4 class="tileTitle">Wallet Balance</h4>
-                <h2><b id="overview_wallet">0.0 BTC</b></h2>
-                <h5 style="color: rgba(0, 0, 0, 0.33);">You have 0 Total Transaction</h5>
-            </div>
-            
-            <div class="w3-container w3-round w3-center w3-card-2 animated fadeInUp w3-col m5 l3  tiles_set" onclick="showMsg('Your total members below you', 'My Downlines', 'Okay');" style="-webkit-animation-delay: 0.2s; background-color: rgb(255, 201, 111); color: white; margin-top: 10px;">
-                <h4 class="tileTitle">My Downlines</h4>
-                <h2><b id="count_Mypeople">0 </b></h2>
-                <h5 id="lastinvite" style="color: rgba(0, 0, 0, 0.33);"></h5>
-            </div>
-            
-            <div class="w3-container w3-round w3-center w3-card-2 animated fadeInUp w3-col m5 l3  tiles_set" onclick="showMsg('Your earnings from your direct referrals', 'Shared Bonus', 'Okay');" style="-webkit-animation-delay: 0.3s; background-color: rgb(255, 201, 111); color: white; margin-top: 10px;">
-                <h4 class="tileTitle">Shared Bonus</h4>
-                <h2><b id="mypeopleBonus">0.0 BTC</b></h2>
-                <h5 style="color: rgba(0, 0, 0, 0.33);" id="shared_bonus_desc"></h5>
-            </div>
-            
-            <div class="w3-container w3-round w3-center w3-card-2 animated fadeInUp w3-col m5 l3  tiles_set" onclick="showMsg('This is your referral link to share', 'My Link', 'Okay');" style="-webkit-animation-delay: 0.3s; background-color: rgb(255, 201, 111); color: white; margin-top: 10px; ">
-                <h4 class="tileTitle">My Link</h4>
-                <h5><b id="userlink" style="word-wrap:break-word"></b></h5>
-                <h5 style="color: rgba(0, 0, 0, 0.33);" id="userlinkDesc"></h5>
-            </div>-->
-
-        </div>
 
 </div>
 
