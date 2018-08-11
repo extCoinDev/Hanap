74e40509a9f04766e5fc9273622524691f656c33
Xeon <jjeeraldo@gmail.com>
2018-08-11 16:52:46 +0800

    bug removed

--- appViews/views_prod-tech.php
+++ appViews/views_prod-tech.php
@@ -1,10 +1,10 @@
 <div id="pages_prod-tech" class="page animated slideInRight" style="animation-duration:.5s!important; overflow: hidden; z-index: 500 !important;width: 100%; padding-top: 0px; height:100vh;">
 
   
-<<<<<<< HEAD
+
   <!--PAGE MENU--> 
     <div class="page_menu" style="background-color: #FFA000; ">
-=======
+
     <!--<div id="pageMenu_login" class="page_menu" style="animation-duration: .5s; background-color: rgba(0, 0, 0, 0); padding-top:35px; padding-left:15px">
         <i class="fa fa-chevron-left" onclick="close_view('pages_login','YES');" style="color:white; font-size:24px"></i>
         <div style="display: inline-block; position:absolute; margin-top:4px; margin-left:5px">
@@ -17,8 +17,7 @@
                 <div id="feed_body" class="feed_body" style="-webkit-animation-duration: .8s !important">
 
     <div class="w3-card-4"  style="background-color: #FFCA28; height: 10%; width:100%;margin-top:-20px;text-align:center;padding-top:5px;color:white;">
->>>>>>> be1cd6a74205c550ddcf63611a616003157a3c4f
-       
+
          <!--MENU TITLE--> 
         <h5 style="margin-left:auto; position: absolute; margin-right:auto; text-align:center; display:inline-block; width:100%; font-size:20px; font-weight:600; margin-top:3px;; margin-top:3px; margin-top:3px;">PRODUCTS & TECHNOLOGY</h5>
 
